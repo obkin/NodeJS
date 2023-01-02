f523a91365fdea24770def7959c232cdcafd90d5
obkin <kapellwork@gmail.com>
2023-01-02 14:12:36 +0200

    lesson-17 - done

--- demo/app.js
+++ demo/app.js
@@ -1,57 +1,24 @@
 const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
 
-/*
-
-async function myEvents() {
-
-    await myEmitter.on('msg', (data) => {
-        console.log(`Отримано: ${data}`)
-    });
-    
-    await myEmitter.prependListener('msg', (data) => {
-        console.log('We used prepend')
-    });
-    
-    
-    await myEmitter.emit('msg', 'Hi, Yan!');
-    
-    await myEmitter.off('msg', (data) => {
-        console.log(`Отримано: ${data}`)
-    });
-
-    await myEmitter.on('connected', () => {
-        console.log('DB was connected');
-    });
-    
-    await myEmitter.emit('connected');
-}
-
-myEvents();
-
-// myEmitter.setMaxListeners(3);
-// console.log('Max listeners: ' + myEmitter.getMaxListeners());
-// console.log('Current num of listeners: ' + myEmitter.listenerCount('msg'));
-
-// console.log(myEmitter.listeners('msg'));
-
-console.log(myEmitter.eventNames());
-
-myEmitter.on('error', (err) => {
-    console.log(`Сталась помилка: ${err.message}`);
+myEmitter.on('connected', () => {
+    console.log('DB was connected.');
 });
 
-myEmitter.emit('error', new Error('BOOOM!'));
+myEmitter.emit('connected');
+    console.log(myEmitter.listenerCount('connected'));
+    console.log(myEmitter.eventNames());
+
+myEmitter.removeAllListeners('connected');
+    console.log(myEmitter.listenerCount('connected'));
 
 
-*/
+myEmitter.once('error', (err) => {
+    if (err) {
+        console.log(`ERROR: ${err.message}`);
+    } else {
+        console.log('Everything us OK!');
+    }
+});
 
-const target = new EventTarget();
-
-const logTarget = () => {
-    console.log('Connected to target');
-};
-
-target.addEventListener('connected', logTarget);
-target.dispatchEvent(new Event('connected'));
-
+myEmitter.emit('error');
