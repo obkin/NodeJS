4207a5d3866620327ad371688ca22a7ef6be9828
mac <mac@MacBook-Air-mac.local>
2023-01-09 13:21:20 +0200

    lesson-21 - start

--- demo/app.js
+++ demo/app.js
@@ -1,64 +1,8 @@
-// const start = performance.now();
-// console.log(`Час на підготовку: ${Math.floor(start)}мс`);
+console.log('Init');
 
-// setTimeout(() => {
-//     console.log(`Повний час: ${Math.floor(performance.now())}мс`);
-//     console.log(`Час роботи таймера: ${Math.floor(performance.now() - start)}мс`);
-//     console.log('Пройшла 1 секунда.');
-// }, 1000);
+setTimeout(() => {
+    console.log(performance.now(), '- Timeout - 0sec');
+}, 0);
 
-
-
-// function user(name) {
-//     console.log(`Ім'я користувача: ${name}`);
-
-// }
-
-// setTimeout(user, 1500, 'Yarik');
-
-
-// const timerId = setTimeout(() => {
-//     console.log('BOOOM!'); 
-// }, 3000);
-
-// setTimeout(() => {
-//     clearTimeout(timerId);
-//     console.log('Was cleared.');
-// }, 1000);
-
-
-
-// const intervalId = setInterval(() => {
-//     console.log(performance.now());
-// }, 1000);
-
-
-// setTimeout(() => {
-//     clearInterval(intervalId);
-//     console.log('Очищено!');
-// }, 5055);
-
-
-
-
-// console.log('Перший');
-
-// setImmediate(() => {
-//     console.log('Другий');
-// });
-
-// console.log('Третій');
-
-
-
-const timerId = setTimeout(() => {
-    console.log('BOOOM!');
-}, 5000);
-
-timerId.unref();
-
-
-setImmediate(() => {
-    timerId.ref();
-});
+console.log('Final');
 
