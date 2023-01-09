268f232525e9d417409870d7992b446d349e7ec5
obkin <kapellwork@gmail.com>
2023-01-09 16:57:32 +0200

    lesson-21 - done

--- demo/app.js
+++ demo/app.js
@@ -1,8 +1,36 @@
+const fs = require('fs');
+
 console.log('Init');
 
 setTimeout(() => {
     console.log(performance.now(), '- Timeout - 0sec');
+
+    Promise.resolve().then(() => {
+        console.log('Promise2');
+    });
+    process.nextTick(() => console.log('nextTick2'));
+}, 100);
+
+setImmediate(() => {
+    console.log('Immediate');
+});
+
+fs.readFile(__filename, () => {
+    console.log('File readed');
+});
+
+setTimeout(() => {
+    for (let i = 0; i < 10_000_000_00; i++) {
+
+    }
+    console.log('done!');
 }, 0);
+
+Promise.resolve().then(() => {
+    console.log('Promise');
+});
+
+process.nextTick(() => console.log('nextTick'));
 
 console.log('Final');
 
