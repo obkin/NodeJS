054fdd958353aa574a8c4e758cd4451270ca0cb5
obkin <kapellwork@gmail.com>
2023-01-17 23:19:09 +0200

    lesson-23 - add: threadpool_size

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
 const start = performance.now();
 
+process.env.UV_THREADPOOL_SIZE = 8;
+
 for (let i = 0; i < 50; i++) {
     crypto.pbkdf2('test', 'salt', 100_000, 64, 'sha512', () => {
         console.log(performance.now() - start);
