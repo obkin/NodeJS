cd2d04f4ab1a2ee73482bf89f57cc322acd9507c
mac <mac@MacBook-Air-mac.local>
2023-01-18 12:50:45 +0200

    lesson-23 - done

--- demo/app.js
+++ demo/app.js
@@ -1,11 +1,21 @@
 const crypto = require('crypto');
+const https = require('https');
 const start = performance.now();
 
-process.env.UV_THREADPOOL_SIZE = 8;
+// process.env.UV_THREADPOOL_SIZE = 2;
+
+// for (let i = 0; i < 50; i++) {
+//     crypto.pbkdf2('test', 'salt', 100_000, 64, 'sha512', () => {
+//         console.log(performance.now() - start);
+//     });
+// }
 
 for (let i = 0; i < 50; i++) {
-    crypto.pbkdf2('test', 'salt', 100_000, 64, 'sha512', () => {
-        console.log(performance.now() - start);
+    https.get('https://youtube.com', (res) => {
+        res.on('data', () => { });
+        res.on('end', () => {
+            console.log(performance.now() - start);
+        });
     });
 }
 
