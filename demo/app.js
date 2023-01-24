bca6d2f11b2bfd89f12c09afb43e369e75013c05
mac <mac@MacBook-Air-mac.local>
2023-01-24 19:10:55 +0200

    lesson-24 - some_changes

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,11 @@
 function slow() {
     performance.mark('start');
-    const arr = [];
 
+    const arr = [];
     for (let i = 0; i < 10_000_000; i++) {
         arr.push(i * i);
     }
+    
     performance.mark('end');
     performance.measure('slow', 'start', 'end');
     console.log(performance.getEntriesByName('slow'));
