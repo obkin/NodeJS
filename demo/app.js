44db87ead106e0f70c0e641b33bf27ba54f4f86a
mac <mac@MacBook-Air-mac.local>
2023-01-26 23:56:50 +0200

    lesson-24 - repeat

--- demo/app.js
+++ demo/app.js
@@ -2,14 +2,17 @@ const perf_hooks = require('perf_hooks');
 
 test = perf_hooks.performance.timerify(test);
 
-const performanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
-    console.log(items.getEntries());
+const PerformanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
+    // console.log(items.getEntries());
     const entry = items.getEntriesByName('slow').pop();
-    console.log(`${entry.name}: ${Math.floor(entry.duration)}мс`);
+    console.log(`${entry.name}: ${Math.floor(entry.duration)}мс.`);
+
+    const testEntry = items.getEntriesByType('function').pop();
+    console.log(`${testEntry.name}: ${Math.floor(testEntry.duration)}мс.`)
     observer.disconnect();
 });
 
-performanceObserver.observe({ entryTypes: ['measure', 'function'] });
+PerformanceObserver.observe({ entryTypes: ['measure', 'function'] });
 
 function test() {
     const arr = [];
@@ -24,14 +27,46 @@ test();
 function slow() {
     performance.mark('start');
     const arr = [];
-
+     
     for (let i = 0; i < 10_000_000; i++) {
         arr.push(i * i);
     }
     performance.mark('end');
     performance.measure('slow', 'start', 'end');
-    // console.log(performance.getEntriesByName('slow'));
 }
 
-slow();  
+slow();
 
+
+
+
+
+
+
+
+
+/*
+
+function slow() {
+    performance.mark('start');
+
+    const arr = [];
+
+    for (let i = 0; i < 10_000_000; i++) {
+        arr.push(i * i);
+    }
+
+    performance.mark('end');
+    performance.measure('slow', 'start', 'end');
+    const measure = performance.getEntriesByName('slow');
+    console.log(getInt(measure.pop().duration));
+}
+
+slow();
+
+
+function getInt(num) {
+    return Math.floor(num);
+}
+
+*/
