172090e4eba338166d8ce7a9413c5edb3a92ba2e
mac <mac@MacBook-Air.local>
2023-02-06 10:35:53 +0200

    lesson-27 - repeated

--- demo-4/app.js
+++ demo-4/app.js
@@ -3,12 +3,13 @@ const { fork } = require('child_process');
 const forkProcess = fork('fork.js');
 
 forkProcess.on('message', (msg) => {
-    console.log(`Main-received: ${msg}`);
+    console.log(`Fork's response: ${msg}`)
 });
 
 forkProcess.on('exit', (code) => {
-    console.log(`Output code: ${code}`);
+    console.log(`Fork exited. Code: ${code}`);
 });
 
 forkProcess.send('Ping');
 forkProcess.send('disconnect');
+
--- demo-4/fork.js
+++ demo-4/fork.js
@@ -2,7 +2,7 @@ process.on('message', (msg) => {
     if (msg === 'disconnect') {
         return process.disconnect();
     }
-    console.log(`Fork-received: ${msg}`);
-    process.send('Pong');
+    console.log(`From fork: ${msg}`);
+    process.send('Pong.');
 });
 
